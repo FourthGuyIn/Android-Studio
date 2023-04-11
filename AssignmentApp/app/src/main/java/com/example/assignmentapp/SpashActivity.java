8c7f05946f95e9bb608ac5673f454ecfb1a26ca8
FourthGuyIn <116372090+FourthGuyIn@users.noreply.github.com>
2023-04-10 22:46:44 -0300

    Hello 1.3

---
+++ AssignmentApp/app/src/main/java/com/example/assignmentapp/SpashActivity.java
@@ -0,0 +1,28 @@
+package com.example.assignmentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SpashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spash);
+
+        Handler handler =new Handler();
+
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SpashActivity.this,Login.class));
+                finish();
+            }
+        }, 5000);
+
+    }
+}
